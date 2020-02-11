d89838159abaff269a1791f0ef58f1fe7bc323d9
Iwan Bowen Morgan <ibmorgan0@gmail.com>
2020-02-11 19:23:08 +0000

    initial commit

---
+++ api/src/main/java/com/ibmorgan/exerciseclinic/ExerciseClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ibmorgan.exerciseclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseClinicApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ibmorgan/exerciseclinic/ExerciseClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmorgan.exerciseclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
