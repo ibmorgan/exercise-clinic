9d6654f9d8e64fbdf7597bd22263421974d783ef
Iwan <iwan.morgan@deep3.co.uk>
2020-04-10 22:18:27 +0100

    Removed un needed exception classes

--- api/src/main/java/com/ibmorgan/exerciseclinic/exception/ExerciseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ibmorgan.exerciseclinic.exception;
-
-public class ExerciseNotFoundException extends RuntimeException {
-    public ExerciseNotFoundException(long id) {
-        super(String.format("Exercise with the ID: %o, not found", id));
-    }
-}
--- api/src/main/java/com/ibmorgan/exerciseclinic/exception/PlanNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ibmorgan.exerciseclinic.exception;
-
-public class PlanNotFoundException extends RuntimeException {
-    public PlanNotFoundException(long id) {
-        super(String.format("Plan with the ID: %o, not found", id));
-    }
-}
